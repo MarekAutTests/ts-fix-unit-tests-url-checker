4b3c91cbfb3526558a6a1906d607c1e0f0465cd1
Marek Vorp <marek.vorp@gmail.com>
2024-11-04 15:20:25 +0200

    [Fix] | Removed unnecessary FIXME annotations

--- tests/url-checker.test.ts
+++ tests/url-checker.test.ts
@@ -11,18 +11,15 @@ describe("LinkChecker", () => {
     expect(linkChecker.isValidUrl("https://www.example.com")).toBe(true);
   });
 
-  // FIXME
   test("should return true for valid URL with http", () => {
     expect(linkChecker.isValidUrl("http://www.example.com"));
   });
 
-  // FIXME
   test("should return false for invalid URL with no protocol", () => {
     expect(linkChecker.isValidUrl("www.example.com")).toBeFalsy();
   });
 
   // FIXME:
-
   // test('should return true for valid URL with port', () => {
   //     linkChecker.isValidUrl('http://www.example.com:8080')).toBetrue;
   // })
